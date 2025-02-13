80b21af3fb9724d2da7499c81456653e80a7c181
Chien-Yao Huang <hk850522@gmail.com>
2025-02-13 19:13:39 +0800

    Update temp_file.cpp

--- LeetCode Question/temp_file.cpp
+++ LeetCode Question/temp_file.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int getDigitSum(int num) {
-        int res = 0;
-        while (num) {
-            res += num % 10;
-            num /= 10;
-        }
-
-        return res;
-    }
-    int maximumSum(vector<int>& nums) {
-        int res = -1;
-        vector<vector<int>> record(50, vector<int>(2, -1));
+    int minOperations(vector<int>& nums, int k) {
+        int n = 0, res = 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
 
         for (int num : nums) {
-            int sum = getDigitSum(num);
-            if (num > record[sum][0]) {
-                record[sum][1] = record[sum][0];
-                record[sum][0] = num;
-            }
-            else if (num > record[sum][1]) {
-                record[sum][1] = num;
-            }
-
-            if (record[sum][0] != -1 && record[sum][1] != -1) {
-                res = max(res, record[sum][0] + record[sum][1]);
+            if (num < k) {
+                pq.push(num);
+                n++;
             }
         }
 
+        while (n >= 2) {
+            int a = pq.top(), b;
+            pq.pop();
+            b = pq.top();
+            pq.pop();
+
+            if (a * 2 + b >= k) {
+                n -= 2;
+            }
+            else {
+                n -= 1;
+                pq.push(a * 2 + b);
+            }
+            res ++;
+        }
+
         return res;
+
     }
 };
