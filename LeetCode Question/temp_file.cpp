506cdffd963fb214c6a8edc49c9f42a8a55b56db
Chien-Yao Huang <hk850522@gmail.com>
2025-03-20 18:53:57 +0800

    Update temp_file.cpp

--- LeetCode Question/temp_file.cpp
+++ LeetCode Question/temp_file.cpp
@@ -1,19 +1,91 @@
 class Solution {
 public:
-    vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n = nums.size(), l = 0, r = n - 1;
-        vector<int> res(n, pivot);
+    int find(vector<int> &disjoint, int x)
+    {
+        return (disjoint[x] == x) ? x : disjoint[x] = find(disjoint, disjoint[x]);
+    }
+
+    void merge(vector<int> &disjoint, vector<int> &min_path, int x, int y, int length)
+    {
+        int fx = find(disjoint, x), fy = find(disjoint, y);
+        if (fx == fy) {
+            min_path[fx] &= length;
+        }
+        else {
+            disjoint[fy] = fx;
+            min_path[fx] &= (length & min_path[fy]);
+        }
+    }
+
+    int find_path(vector<int> &disjoint, vector<int> &min_path, int x, int y)
+    {
+        int fx = find(disjoint, x), fy = find(disjoint, y);
+        if (fx == fy) return min_path[fx];
+        return -1;
+    }
+
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        vector<int> disjoint(n, 0), min_path(n, -1);
+        vector<int> res;
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] < pivot) {
-                res[l++] = nums[i];
-            }
+            disjoint[i] = i;
         }
 
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] > pivot) {
-                res[r--] = nums[i];
-            }
+        for (int i = 0; i < edges.size(); i++) {
+            int x = edges[i][0], y = edges[i][1], len = edges[i][2];
+            merge(disjoint, min_path, x, y, len);
+        }
+
+        for (int i = 0; i< query.size(); i++) {
+            int x = query[i][0], y = query[i][1];
+            res.push_back(find_path(disjoint, min_path, x, y));
+        }
+
+        return res;
+    }
+};class Solution {
+public:
+    int find(vector<int> &disjoint, int x)
+    {
+        return (disjoint[x] == x) ? x : disjoint[x] = find(disjoint, disjoint[x]);
+    }
+
+    void merge(vector<int> &disjoint, vector<int> &min_path, int x, int y, int length)
+    {
+        int fx = find(disjoint, x), fy = find(disjoint, y);
+        if (fx == fy) {
+            min_path[fx] &= length;
+        }
+        else {
+            disjoint[fy] = fx;
+            min_path[fx] &= (length & min_path[fy]);
+        }
+    }
+
+    int find_path(vector<int> &disjoint, vector<int> &min_path, int x, int y)
+    {
+        int fx = find(disjoint, x), fy = find(disjoint, y);
+        if (fx == fy) return min_path[fx];
+        return -1;
+    }
+
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        vector<int> disjoint(n, 0), min_path(n, -1);
+        vector<int> res;
+
+        for (int i = 0; i < n; i++) {
+            disjoint[i] = i;
+        }
+
+        for (int i = 0; i < edges.size(); i++) {
+            int x = edges[i][0], y = edges[i][1], len = edges[i][2];
+            merge(disjoint, min_path, x, y, len);
+        }
+
+        for (int i = 0; i< query.size(); i++) {
+            int x = query[i][0], y = query[i][1];
+            res.push_back(find_path(disjoint, min_path, x, y));
         }
 
         return res;
